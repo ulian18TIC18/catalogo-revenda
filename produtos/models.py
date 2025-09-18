b281f08fe092f7197984f07f29ce6fae1db926cd
ulian18TIC18 <ulian18@gmail.com>
2025-09-18 20:19:39 -0300

    criado modelo Produto

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='imagens/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.nome

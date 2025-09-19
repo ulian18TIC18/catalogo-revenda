a0291ca27b8ae8c3dbd5ddfea1d6b6fe91e42740
ulian18TIC18 <ulian18@gmail.com>
2025-09-19 20:32:13 -0300

    Cadastro pela plataforma adm do Django

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from produtos.models import Produto
+
+@admin.register(Produto)
+class ProdutoAdmim(admin.ModelAdmin):
+    list_display = ("nome", "descricao", "preco", "imagem")
+    search_fields = ("nome", )

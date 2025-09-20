b42c4fefd2024ff0c0ccc8594e54f104954b211a
ulian18TIC18 <ulian18@gmail.com>
2025-09-19 21:23:39 -0300

    Correcao - path imagens

--- produtos/models.py
+++ produtos/models.py
@@ -6,7 +6,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    imagem = models.ImageField(upload_to='imagens/', blank=True, null=True)
+    imagem = models.ImageField(upload_to='imagens_produtos/', blank=True, null=True)
     
     def __str__(self):
         return self.nome
